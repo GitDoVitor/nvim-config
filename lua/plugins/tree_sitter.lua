26407b08a867614422af9ef0106dcb0909839f8c
João Vitor <joaovitorsantoscs@gmail.com>
2025-03-01 00:50:19 -0300

    ajustando plugins

--- lua/plugins/tree_sitter.lua
+++ lua/plugins/tree_sitter.lua
@@ -5,7 +5,25 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "json", "xml", "yaml" },
+      ensure_installed = { 
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml", 
+        "c",
+        "vimdoc",
+        "xml", 
+      },
       sync_install = true,
       highlight = { enable = true, },
       indent = { enable = true, },
